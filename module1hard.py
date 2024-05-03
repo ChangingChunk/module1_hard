c9ec3ace9916c11715e9951c9b73be4ef1e86e80
ChangingChunk <38633470+ChangingChunk@users.noreply.github.com>
2024-05-03 12:14:47 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,16 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2],
+          [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+alph_students = sorted(list(students))
+
+grades_1 = sum(grades[0]) / len(grades[0])
+grades_2 = sum(grades[1]) / len(grades[1])
+grades_3 = sum(grades[2]) / len(grades[2])
+grades_4 = sum(grades[3]) / len(grades[3])
+grades_5 = sum(grades[4]) / len(grades[4])
+
+average_grades = [grades_1, grades_2, grades_3, grades_4, grades_5]
+
+average_score = dict(zip(alph_students, average_grades))
+print(average_score)
